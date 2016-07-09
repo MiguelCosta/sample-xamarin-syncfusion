34a4db19693944c7f077ead1174eb3f1b71064c1
Miguel Costa <miguelpintodacosta@gmail.com>
2016-07-10 00:38:40 +0100

    NumericUpDown working

--- Mpc.XamSyncfusion/Mpc.XamSyncfusion/Mpc.XamSyncfusion/MainPage.xaml.cs
+++ Mpc.XamSyncfusion/Mpc.XamSyncfusion/Mpc.XamSyncfusion/MainPage.xaml.cs
@@ -12,7 +12,15 @@ namespace Mpc.XamSyncfusion
 
         private async void btnSfNumericUpDownClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new Views.SfNumericUpDown());
+            try
+            {
+                var p = new Views.NumericUpDown();
+                await Navigation.PushAsync(p);
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "ok");
+            }
         }
     }
 }
---
+++ Mpc.XamSyncfusion/Mpc.XamSyncfusion/Mpc.XamSyncfusion/Views/NumericUpDown.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Syncfusion.SfNumericUpDown.XForms;
+using Xamarin.Forms;
+
+namespace Mpc.XamSyncfusion.Views
+{
+    public partial class NumericUpDown : ContentPage
+    {
+        public NumericUpDown()
+        {
+            InitializeComponent();
+            var numeric = new SfNumericUpDown();
+            numeric.FormatString = "c";
+            numeric.Culture = new System.Globalization.CultureInfo("pt-PT");
+            stcMain.Children.Add(numeric);
+        }
+    }
+}
